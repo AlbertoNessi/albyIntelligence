7f37069654b0c65cc07b8df4093b0fe246e8d06a
Alberto <alberto.nessi@secgroup.it>
2024-11-02 17:53:46 +0100

    More security

--- src/Controller/AlbyIntelligenceImageAnalysisController.php
+++ src/Controller/AlbyIntelligenceImageAnalysisController.php
@@ -4,28 +4,41 @@ namespace App\Controller;
 
 use App\Services\AIPromptResponseService;
 use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Services\ChatGPTService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class AlbyIntelligenceImageAnalysisController extends AbstractController
 {
     private ChatGPTService $chatGPTService;
     private AIPromptResponseService $aiPromptResponseService;
+    private CsrfTokenManagerInterface $csrfTokenManager;
+    private Loggerinterface $logger;
 
-    public function __construct(ChatGPTService $chatGPTService, AIPromptResponseService $aiPromptResponseService)
+    public function __construct(ChatGPTService $chatGPTService, AIPromptResponseService $aiPromptResponseService, CsrfTokenManagerInterface $csrfTokenManager, LoggerInterface $logger)
     {
         $this->chatGPTService = $chatGPTService;
         $this->aiPromptResponseService = $aiPromptResponseService;
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->logger = $logger;
     }
 
     #[Route('/analyze-image', name: 'analyzeImage_url', methods: ['POST'])]
     public function analyzeImage(Request $request): JsonResponse
     {
+        $token = $request->request->get('_token');
+        if (!$this->isCsrfTokenValid('update_index', $token)) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => 'Invalid CSRF token.'
+            ], Response::HTTP_FORBIDDEN);
+        }
+
         $imageFile = $request->files->get('image');
         $prompt = $request->request->get('prompt', 'Describe the image');
 
@@ -41,20 +54,10 @@ class AlbyIntelligenceImageAnalysisController extends AbstractController
             $messages = $this->aiPromptResponseService->generatePromptForImageAnalysis($prompt, $dataUrl);
             $response = $this->chatGPTService->sendImageRequest($messages);
 
-            return new JsonResponse([
-                'code' => '200',
-                'response' => $response
-            ]);
-        } catch (TransportExceptionInterface|Exception $e) {
-            return new JsonResponse([
-                'code' => $e->getCode(),
-                'message' => [
-                    'line' => $e->getLine(),
-                    'message' => $e->getMessage(),
-                    'trace' => $e->getTraceAsString(),
-                    'file' => $e->getFile()
-                ]
-            ]);
+            return new JsonResponse(['response' => $response], 200);
+        } catch (Exception $e) {
+            $this->logger->error('Error analyzing image: ' . $e->getMessage());
+            return new JsonResponse(['error' => 'An error occurred while processing your request'], 500);
         }
     }
 }
